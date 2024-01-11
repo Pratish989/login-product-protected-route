25dd626aa91286938679c8e1f3c49201d615868b
Pratish <75656608+Pratish989@users.noreply.github.com>
2024-01-11 19:04:49 +0530

    Update LogoutPage.jsx

--- src/pages/public/LogoutPage.jsx
+++ src/pages/public/LogoutPage.jsx
@@ -1,28 +1,34 @@
 /* eslint-disable no-unused-vars */
 import { useAuth } from "../../hooks/useAuth";
-import { useNavigate } from "react-router-dom";
+import { useNavigate , Navigate} from "react-router-dom";
 import { useState } from "react";
+import LoginPage from "./LoginPage";
 
 const LogoutPage = () => {
-  const { logout } = useAuth();
-  const navigate = useNavigate();
-  const [token, setToken] = useState(localStorage.getItem("token"));
+const { logout } = useAuth();
+const navigate = useNavigate();
+const [token, setToken] = useState(localStorage.getItem("token"));
 
-  function handleLogout() {
-    logout();
-    localStorage.clear();
-    navigate("/login");
-    setToken("");
-    console.log(token);
-  }
-  return (
-    <>
-      <div>LogoutPage</div>
-      <button onClick={handleLogout} style={{ color: "blue" }}>
-        Logout
-      </button>
-    </>
-  );
+function handleLogout() {
+  logout();
+  localStorage.clear();
+  // <Navigate to={<LoginPage/>}/>
+  navigate("/login");
+  setToken("");
+  // location.reload()
+  navigate(0)
+  console.log(token);
+}
+return (
+  <>
+    <div>LogoutPage</div>
+    <button onClick={handleLogout} style={{ color: "blue" }}>
+      Logout
+    </button>
+  </>
+);
 };
 
 export default LogoutPage;
+
+
