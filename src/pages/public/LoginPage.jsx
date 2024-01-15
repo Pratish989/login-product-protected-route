c24a44d914a1d379055b126f9cad2059ceaec357
Pratish <75656608+Pratish989@users.noreply.github.com>
2024-01-15 18:16:11 +0530

    Update LoginPage.jsx

--- src/pages/public/LoginPage.jsx
+++ src/pages/public/LoginPage.jsx
@@ -1,16 +1,10 @@
-/* eslint-disable no-unused-vars */
-import { useEffect, useState } from "react";
-import { useAuth } from "../../hooks/useAuth";
-import { useNavigate, redirect } from "react-router-dom";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
-let staticEmail = "admin";
-
-function LoginPage() {
+function LoginPage({ onLogin }) {
   const navigate = useNavigate();
   const [email, setEmail] = useState("");
   const [error, setError] = useState(false);
-  const [token,setToken] = useState(" ")
-  console.log(token)
 
   const handleChange = (event) => {
     setEmail(event.target.value);
@@ -18,18 +12,15 @@ function LoginPage() {
 
   const handleLogin = (event) => {
     event.preventDefault();
-    if (email === staticEmail) {
-      setToken("login token")
-      setToken(localStorage.setItem("token","login token"))
+    if (email === "admin") {
+      onLogin();
       setError(false);
-      console.log(token)
       navigate("/home");
-      navigate(0)
-      history.push()
     } else {
       setError(true);
     }
-  };    
+  };
+
   return (
     <div>
       <label htmlFor="email">Email Address</label>
@@ -42,5 +33,7 @@ function LoginPage() {
     </div>
   );
 }
+
 export default LoginPage;
+
 
